6c1922aa2fe22ce0487fe7f38f67355b962af100
anhmv <anhcdcn98@gmail.com>
2021-07-18 17:33:30 +0700

    unset fb

--- resources/views/modules/footer.blade.php
+++ resources/views/modules/footer.blade.php
@@ -266,28 +266,8 @@
 <script type="text/javascript" src="assets/js/saiko-custom.js"></script>
 
 
-<div class="fb-customerchat" page_id="346391759462555" theme_color="#fa3c4c"
-    logged_in_greeting="Xin chào ! Hãy để Saiko giúp bạn!"
-    logged_out_greeting="Chào bạn! Hãy đăng nhập facebook để kết nối với Saiko">
-</div>
-
 <div id="fb-root"></div>
 <script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml: true,
-            version: 'v3.3'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
 
     function track() {
         $("#alert_footer").hide()
