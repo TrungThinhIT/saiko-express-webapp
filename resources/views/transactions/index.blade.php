5f787accbd4fe3be7c7acc111a8dc5af6143941f
anhmv <anhcdcn98@gmail.com>
2021-09-10 16:00:27 +0700

    fix view detail account

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -1,23 +1,23 @@
 @extends('modules_manager.main_new')
 @section('title', 'Tài khoản tiền')
-@section('title-header-content','Chi tiết tài khoản')
+@section('title-header-content', 'Chi tiết tài khoản')
 @section('css')
     <style>
         .row .col-sm-8 input {
             font-weight: 600;
         }
-        .form-group label{
+
+        .form-group label {
             white-space: unset !important;
         }
+
     </style>
 @section('content')
-    {{-- {{ dd($data) }} --}}
     @if (isset($data['transactions']))
         <div class="col-md-12">
             <div class="row">
                 @if (count($data['transactions']['data']))
                     @foreach ($data['transactions']['data'] as $key => $value)
-                        {{-- {{dd($value)}} --}}
                         <div class="col-md-6">
                             <div class="card fix-overflow">
                                 <div class="card-header ">
@@ -26,10 +26,18 @@
                                 <div class="card-body custom-background set-overflow">
                                     <form>
                                         <div class="form-group row align-items-center">
-                                            <label for="order-id" class="col-sm-4 col-form-label">Số dư</label>
+                                            <label for="order-id" class="col-sm-4 col-form-label">Số dư trong tài
+                                                khoản</label>
                                             <div class="col-sm-8">
                                                 <input type="text" readonly class="form-control"
                                                     value="{{ number_format($value['balance']) ? number_format($value['balance']) . '   ' . $value['currency']['symbol'] : '' }}">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row align-items-center">
+                                            <label for="order-id" class="col-sm-4 col-form-label">Số dư đầu kỳ</label>
+                                            <div class="col-sm-8">
+                                                <input type="text" readonly class="form-control"
+                                                    value="{{ number_format($value['opening_balance']) ? number_format($value['opening_balance']) . '   ' . $value['currency']['symbol'] : '' }}">
                                             </div>
                                         </div>
                                         <div class="form-group row align-items-center">
@@ -40,42 +48,46 @@
                                             </div>
                                         </div>
                                         <div class="form-group row align-items-center">
-                                            <label for="status" class="col-sm-4 col-form-label">Tiền thanh toán đơn bán
-                                                hàng</label>
+                                            <label for="status" class="col-sm-4 col-form-label">Tiền đơn hàng đã ghi
+                                                nợ</label>
                                             <div class="col-sm-8">
                                                 <input type="text" readonly class="form-control"
                                                     value="{{ number_format($value['order']) ? number_format($value['order']) . '   ' . $value['currency']['symbol'] : '' }}">
                                             </div>
                                         </div>
                                         <div class="form-group row align-items-center">
-                                            <label for="insuarance-price" class="col-sm-4 col-form-label">Tiền thanh toán
-                                                đơn
-                                                mua
-                                                hàng</label>
+                                            <label for="status" class="col-sm-4 col-form-label">Tiền đơn hàng đã thanh toán
+                                            </label>
                                             <div class="col-sm-8">
                                                 <input type="text" readonly class="form-control"
-                                                    value="{{ number_format($value['purchase']) ? number_format($value['purchase']) . '   ' . $value['currency']['symbol'] : '' }}">
+                                                    value="{{ number_format($value['payment_order']) ? number_format($value['payment_order']) . '   ' . $value['currency']['symbol'] : '' }}">
                                             </div>
                                         </div>
                                         <div class="form-group row align-items-center">
-                                            <label for="special-goods-price" class="col-sm-4 col-form-label">Tiền thanh toán
-                                                dịch
-                                                vụ</label>
+                                            <label for="special-goods-price" class="col-sm-4 col-form-label">Tiền dịch vụ đã
+                                                ghi nợ</label>
                                             <div class="col-sm-8">
                                                 <input type="text" readonly class="form-control"
                                                     value="{{ number_format($value['service']) ? number_format($value['service']) . '   ' . $value['currency']['symbol'] : '' }}">
                                             </div>
                                         </div>
-                                        {{-- <div class="form-group row align-items-center">
-                                            <label for="trip-id" class="col-sm-4 col-form-label">Tổng tiền thanh
-                                                toán</label>
+                                        <div class="form-group row align-items-center">
+                                            <label for="special-goods-price" class="col-sm-4 col-form-label">Tiền dịch vụ đã
+                                                thanh toán</label>
                                             <div class="col-sm-8">
                                                 <input type="text" readonly class="form-control"
-                                                    value="{{ number_format($value['total_debit']) ? number_format($value['total_debit']) . '   ' . $value['currency']['symbol'] : '' }}">
+                                                    value="{{ number_format($value['payment_service']) ? number_format($value['payment_service']) . '   ' . $value['currency']['symbol'] : '' }}">
                                             </div>
-                                        </div> --}}
+                                        </div>
                                         <div class="form-group row align-items-center">
-                                            <label for="money-refund-jpy" class="col-sm-4 col-form-label">Tổng tiền hoàn
+                                            <label for="trip-id" class="col-sm-4 col-form-label">Tiền đã trả lại</label>
+                                            <div class="col-sm-8">
+                                                <input type="text" readonly class="form-control"
+                                                    value="{{ number_format($value['withdraw']) ? number_format($value['withdraw']) . '   ' . $value['currency']['symbol'] : '' }}">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row align-items-center">
+                                            <label for="money-refund-jpy" class="col-sm-4 col-form-label">Tiền được hoàn
                                                 trả</label>
                                             <div class="col-sm-8">
                                                 <input type="text" readonly class="form-control"
@@ -87,7 +99,6 @@
                                 </div>
 
                             </div>
-
                         </div>
                     @endforeach
                 @endif
@@ -95,15 +106,14 @@
         </div>
     @endif
     <script>
-
-    $(document).ready(function() {
-        $(document).ajaxStart(function() {
-            $("#loader").show();
+        $(document).ready(function() {
+            $(document).ajaxStart(function() {
+                $("#loader").show();
+            });
+            $(document).ajaxStop(function() {
+                $("#loader").hide();
+            });
         });
-        $(document).ajaxStop(function() {
-            $("#loader").hide();
-        });
-    });
 
     </script>
 
